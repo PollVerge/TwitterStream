85eb18d5862d4036c930d02d661a5659575ca17a
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2018-05-22 20:32:13 -0400

    Add inital scrape, dump and process functionality
    start csv processing
    
    update gitignore
    
    formatting
    
    update scraping and processing
    
    remove plotting

---
+++ test_process.py
@@ -0,0 +1,11 @@
+import process
+import pytest
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    ({'text': 'This tweet contains the name Trump'}, 'trump'),
+    ({'text': 'This tweet contains the name Trump and Clinton'}, None),
+    ({'text': 'This tweet contains the name Drump and Blinton'}, None),
+])
+def test_get_candidates(test_input, expected):
+    assert process.get_candidates(test_input) == expected
