57859fa4f8c66ec49ebfa7db9c79279aef36fa26
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2018-06-03 22:30:14 -0400

    Add inital scrape, dump and process functionality (#1)

---
+++ settings.py
@@ -0,0 +1,6 @@
+TRACK_TERMS = ["trump", "clinton", "sanders",
+               "hillary clinton", "bernie", "donald trump"]
+CSV_NAME = "tweets.csv"
+TABLE_NAME = "election"
+
+from private import TWITTER_KEY, TWITTER_SECRET, TWITTER_APP_KEY, TWITTER_APP_SECRET, CONNECTION_STRING
